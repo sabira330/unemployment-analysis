24628374ba99fbecd6f41bab54bbfce41849a8d3
sabira330 <139349514+sabira330@users.noreply.github.com>
2023-07-17 13:07:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/unemployment.csv")
+print(data.head())
